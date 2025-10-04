3f8ecc9dffe10fa2cb558127a38a81a0fa70ff7a
amargoum <achrafmargouma@gmail.com>
2025-10-04 16:33:22 +0100

    start project and create main

--- Jaikin/Main.java
+++ Jaikin/Main.java
@@ -1,5 +1,14 @@
-package Jaikin;
+import javax.swing.JFrame;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        DrawingPanel frame = new DrawingPanel();
+        JFrame window = new JFrame();
+        window.setTitle("Jaikin");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(frame);
+        window.pack();
+        window.setVisible(true);
+        
+    }
 }
